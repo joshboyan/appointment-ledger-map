cd35df5c100ed374f5567a6d67c6e38046e1d1a2
joshboyan <joshboyan@yahoo.com>
2017-06-21 09:30:11 -0700

    Create data model for API

--- src/App.js
+++ src/App.js
@@ -8,35 +8,7 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      appointments: [
-        {
-          appointmentTitle: 'first appointment',
-          appointmentDate: '2/22/16',
-          appointmentTime: '1:00pm',
-          appointmentDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla placeat rerum accusamus, obcaecati eum? Ea porro vel laborum molestias harum perspiciatis tempora, voluptas facilis in facere, nemo ab, possimus vitae.',
-          appointmentDestination: '2940 Hoo Doo Loop Oldtown, ID',
-          appointmentOrigin: '2001 NE 80th Ave Portland, OR',
-          travelMode: 'DRIVING'
-        },
-        {
-          appointmentTitle: 'second appointment',
-          appointmentDate: '3/2/16',
-          appointmentTime: '4:00pm',
-          appointmentDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla placeat rerum accusamus, obcaecati eum? Ea porro vel laborum molestias harum perspiciatis tempora, voluptas facilis in facere, nemo ab, possimus vitae.',
-          appointmentDestination: '2001 NE 80th Ave Portland, OR 97213',
-          appointmentOrigin: 'Imperial Beach San Diego, CA',
-          travelMode: 'DRIVING'
-        },
-        {
-          appointmentTitle: 'third appointment',
-          appointmentDate: '4/26/16',
-          appointmentTime: '7:00pm',
-          appointmentDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla placeat rerum accusamus, obcaecati eum? Ea porro vel laborum molestias harum perspiciatis tempora, voluptas facilis in facere, nemo ab, possimus vitae.',
-          appointmentDestination: '80 Beach Ave Manahawkin, NJ 08050',
-          appointmentOrigin: 'Rock Island, IL',
-          travelMode: 'DRIVING'
-        }
-      ],
+      appointments: [],
       filteredAppointments: []
     };
     this.updateAppointments = this.updateAppointments.bind(this);
